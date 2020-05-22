5d84b28a4806559e2a0618dc7d456b26ed776d46
Mateusz <matiroganowicz@gmail.com>
2020-05-22 19:53:17 +0200

    progress on Add Recipe page

--- CookIT/AddRecipe.xaml.cs
+++ CookIT/AddRecipe.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Security.Cryptography;
 
 namespace CookIT
 {
@@ -20,9 +22,38 @@ namespace CookIT
     /// </summary>
     public partial class AddRecipe : Page
     {
+        
         public AddRecipe()
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox dynamicTextBox = new TextBox();
+            dynamicTextBox.Text = "Type Partnumber";
+            Grid.SetRow(dynamicTextBox, 1);
+            Grid.SetColumn(dynamicTextBox, 0);
+            //this.MainGrid.Children.Add(dynamicTextBox);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Label label = new Label();
+            TextBox tb = new TextBox();
+            tb.Name = "RecipeName";
+            tb.FontSize = 15;
+            label.Margin = new Thickness(0, 20, 0, 0);
+            label.FontSize = 15;
+            label.FontWeight = FontWeights.Bold;
+            label.Content = "Enter ingredient: ";
+            someStack.Children.Add(label);
+            someStack.Children.Add(tb);
+            
+        }
+
+        
     }
+
+
 }
