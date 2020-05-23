b30fc1eb2d6206da05d2a0329e218f7a44723c04
Ruben Menino <33597504+rubenmenino@users.noreply.github.com>
2020-05-24 00:44:37 +0100

    contacUs done with new window

--- CookIT/contactUs.xaml.cs
+++ CookIT/contactUs.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CookIT
 {
     /// <summary>
-    /// Interaction logic for Help.xaml
+    /// Interaction logic for contactUs.xaml
     /// </summary>
-    public partial class Help : UserControl
+    public partial class contactUs : Window
     {
-        public Help()
+        public contactUs()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            contactUs con = new contactUs();
-            con.Show();
         }
     }
 }
