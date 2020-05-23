fc8b7e30940782b507f2459b28825524d93ed30e
Bruno Caseiro <caseiro@ua.pt>
2020-05-23 19:01:30 +0100

    help

--- CookIT/Help.xaml.cs
+++ CookIT/Help.xaml.cs
@@ -25,11 +25,6 @@ namespace CookIT
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
            
