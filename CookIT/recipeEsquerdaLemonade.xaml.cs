a46cf636f58397fcfc189ff94ae0adef5a43d4e8
Ruben Menino <33597504+rubenmenino@users.noreply.github.com>
2020-05-24 01:47:52 +0100

    lemonade done

---
+++ CookIT/recipeEsquerdaLemonade.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CookIT
+{
+    /// <summary>
+    /// Interaction logic for recipeEsquerdaLemonade.xaml
+    /// </summary>
+    public partial class recipeEsquerdaLemonade : UserControl
+    {
+        public recipeEsquerdaLemonade()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            lemonadeRecipe lemo = new lemonadeRecipe();
+            lemo.Show();
+        }
+    }
+}
